2325955f6015094ccb2d0b8a3a4ea6991143dfb4
SandeepIT1 <sandeep1701sa@gmail.com>
2026-08-17 20:08:35 +0530

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -37,7 +37,7 @@ VALIDATE(){
    
 
 
-   cp mongo.repo /etc/yum.repos.d/mongo.repo
+   cp mongo.repo /etc/yum.repos.d/mongodb.repo
    VALIDATE $? "copying mongo repo"
    dnf install mongo-org -y
    VALIDATE $? "installing mongodb server"
@@ -49,4 +49,3 @@ VALIDATE $?" start mongodb"
  VALIDATE "allowing remote connection"
  systemctl restart mongodb
  VALIDATE "restart the mongodb"
- 
