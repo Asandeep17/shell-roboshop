2a573b41f41cb2f28d733c8afe3a1dc8dd3f16c1
SandeepIT1 <sandeep1701sa@gmail.com>
2026-08-18 19:19:18 +0530

    actions

--- mangodb.sh
+++ mangodb.sh
@@ -1,9 +1,6 @@
-
 #!/bin/bash
 
-
 USERID=$(id -u)
-
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
@@ -12,37 +9,35 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ]; then 
-echo -e " $R please run this script with root user access $N" | tee -a $LOG_FILE
-exit 1
-
+    echo -e " $R please run this script with root user access $N" | tee -a $LOG_FILE
+    exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
-
-
-
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
-    echo -e "$2 ...$R FAILURE $N " |tee -a $LOG_FILE
-    exit 1 
+      echo -e "$2 ...$R FAILURE $N " |tee -a $LOG_FILE
+      exit 1 
     else 
         echo -e "$2 ... $G SUCCESS $N" |tee -a $LOG_FILE
-        fi
-
+    fi
 }
+    
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying Mongo repo"
    
+dnf install mongodb-org -y
+VALIDATE $? "installing mongodb server"
+   
+systemctl enable mongod
+VALIDATE $?"enable MongoDB "
 
-
-   cp mongo.repo /etc/yum.repos.d/mongo.repo
-   VALIDATE $? "copying mongo repo"
-   dnf install mongodb-org -y
-   VALIDATE $? "installing mongodb server"
-   systemctl enable mongod
-VALIDATE $?"enable mongodb "
 systemctl start mongod 
-VALIDATE $?" start mongodb"
+VALIDATE $?" start Mongodb"
+
  sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
  VALIDATE "allowing remote connection"
+
  systemctl restart mongod
  VALIDATE "restart the mongodb"
